3a8eef989df8a4466cd0c087eaa32baa40833390
Madokka1 <152441132+Madokka1@users.noreply.github.com>
2026-02-28 15:38:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,27 +20,23 @@ DB_URI = os.getenv("DATABASE_URL")
 @app.get("/busy-dates")
 async def get_dates():
     try:
-        # Проверка: установлена ли переменная
         if not DB_URI:
-            return {"error": "DATABASE_URL is not set"}
-            
+            print("ОШИБКА: DATABASE_URL не настроена в Render")
+            return []
+
         conn = psycopg2.connect(DB_URI)
         cur = conn.cursor(cursor_factory=RealDictCursor)
-        cur.execute("SELECT booking_date FROM appointments WHERE booking_date IS NOT NULL")
-        rows = cur.fetchall()
+        cur.execute("SELECT TO_CHAR(booking_date, 'YYYY-MM-DD HH24:MI') as slot FROM appointments WHERE booking_date IS NOT NULL")
         
-        # Форматируем даты, проверяя, что они существуют
-        dates = []
-        for row in rows:
-            if row['booking_date']:
-                dates.append(row['booking_date'].strftime('%Y-%m-%d'))
+        rows = cur.fetchall()
+        dates = [row['slot'] for row in rows]
         
         cur.close()
         conn.close()
         return dates
     except Exception as e:
-        print(f"ОШИБКА БАЗЫ: {e}") # Это отобразится в логах Render
-        return [] # Возвращаем пустой список вместо падения сервера (500)
+        print(f"ОШИБКА БАЗЫ: {e}")
+        return []
 
 @app.post("/webhook")
 async def handle_tilda(
